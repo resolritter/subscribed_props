7b43b4ee00e1ae6f3f50ca51825714d4edbd10c4
resolritter <joao.paulo.silvasouza@hotmail.com>
2020-05-27 15:53:43 -0300

    attempt better naming
    do not require uniqueId to be explicitly set from the parent

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React, { useRef, useState, useEffect } from "react"
 import "./App.css"
 
-function counterMessageOf(renderCount, prepend = "") {
+function makeCounterMessage(renderCount, prepend = "") {
   return `${prepend}I've been called ${renderCount} times.`
 }
 
+let subscriptionCount = 0
 // High-order component which sets up the subscription on behalf it child
-function bindOf(HookedComponent) {
+function makeSubscriber(PureComponent) {
   return React.memo(
-    function(props) {
-      const [value, setValue] = useState(props.initialValue)
-      props.subscribe(props.uniqueId, setValue)
+    function({ subscribe, initialValue }) {
+      const [uniqueId, setUniqueId] = useState()
+      const [value, setValue] = useState(initialValue)
+      useEffect(
+        function() {
+          const uniqueId = ++subscriptionCount
+          setUniqueId(uniqueId)
+          subscribe(uniqueId, setValue)
+        },
+        [subscribe],
+      )
 
-      return <HookedComponent value={value} uniqueId={props.uniqueId} />
+      return uniqueId ? (
+        <PureComponent value={value} uniqueId={uniqueId} />
+      ) : null
     },
     function alwaysSkipRenderFromProps() {
       return true
@@ -21,13 +32,13 @@ function bindOf(HookedComponent) {
 }
 
 const renderCounter = new Map()
-function useRenderCounter(uniqueId) {
+function bumpRenderCount(uniqueId) {
   const renderCount = (renderCounter.get(uniqueId) || 0) + 1
   renderCounter.set(uniqueId, renderCount)
   return renderCount
 }
 
-function useBind(initialValue) {
+function useSubscription(initialValue) {
   const [value, setValue] = useState(initialValue)
   // `Map` is used instead of an object because it provides dynamic lookup _on
   // demand_, as opposed to an object, where its value would be captured by the
@@ -51,29 +62,29 @@ function useBind(initialValue) {
   ]
 }
 
-const Tock = bindOf(function({ value, uniqueId }) {
-  const renderCount = useRenderCounter(uniqueId)
+const Tock = makeSubscriber(function({ value, uniqueId }) {
+  const renderCount = bumpRenderCount(uniqueId)
   return (
     <div>
-      <div>{counterMessageOf(renderCount, `Tock #${value} received. `)}</div>
+      <div>{makeCounterMessage(renderCount, `Tock #${value} received. `)}</div>
     </div>
   )
 })
 
-const Tick = bindOf(function({ value, uniqueId }) {
-  const renderCount = useRenderCounter(uniqueId)
+const Tick = makeSubscriber(function({ value, uniqueId }) {
+  const renderCount = bumpRenderCount(uniqueId)
   return (
     <div>
-      <div>{counterMessageOf(renderCount, `Tick #${value} received. `)}</div>
+      <div>{makeCounterMessage(renderCount, `Tick #${value} received. `)}</div>
     </div>
   )
 })
 
 let tickTock = 0
 function App() {
-  const [tick, setTick, subscribeToTick] = useBind(0)
-  const [tock, setTock, subscribeToTock] = useBind(0)
-  const renderCount = useRenderCounter("app")
+  const [tick, setTick, subscribeToTick] = useSubscription(0)
+  const [tock, setTock, subscribeToTock] = useSubscription(0)
+  const renderCount = bumpRenderCount("app")
 
   // `setTock` and `setTick` will trigger an update every two seconds;
   // This would otherwise cause the whole tree to be re-rendered, but
@@ -97,14 +108,14 @@ function App() {
   return (
     <div className="App">
       <b>Ticker (Child)</b>
-      <Tick subscribe={subscribeToTick} initialValue={tick} uniqueId={1} />
+      <Tick subscribe={subscribeToTick} initialValue={tick} />
       <b>Ticker (Child 2)</b>
-      <Tick subscribe={subscribeToTick} initialValue={tick} uniqueId={2} />
+      <Tick subscribe={subscribeToTick} initialValue={tick} />
       <b>Tocker (Child)</b>
-      <Tock subscribe={subscribeToTock} initialValue={tick} uniqueId={3} />
+      <Tock subscribe={subscribeToTock} initialValue={tock} />
       <b>Parent</b>
       <br />
-      {counterMessageOf(renderCount)}
+      {makeCounterMessage(renderCount)}
       <hr />
       <p style={{ marginBottom: 0 }}>The example above showcases:</p>
       <ul style={{ listStyle: "inside", margin: 0 }}>
